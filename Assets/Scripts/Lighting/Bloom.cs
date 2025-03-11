de03df1c76abe8145b55b1c33135aee21e6dce0b
Michael <michael.feldman09@gmail.com>
2025-03-11 14:47:04 -0400

    Add bloom post processing

---
+++ Assets/Scripts/Lighting/Bloom.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Bloom : MonoBehaviour
+{
+    public Shader bloomShader;
+    private Material _bloomMat;
+    [Range(0.1f, 1f)]
+    public float bloomScale;
+
+    [Range(1, 10)]
+    public int bloomRadius;
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if(_bloomMat == null)
+        {
+            _bloomMat = new Material(bloomShader);
+        }
+        
+        RenderTexture tmp = RenderTexture.GetTemporary((int)(src.width * bloomScale), (int)(src.height * bloomScale));
+        RenderTexture tmp2 = RenderTexture.GetTemporary(tmp.width, tmp.height);
+        _bloomMat.SetInt("_BloomRadius", bloomRadius);
+        Graphics.Blit(src, tmp, _bloomMat, 0);
+        Graphics.Blit(tmp, tmp2, _bloomMat, 1);
+        Graphics.Blit(tmp2, tmp, _bloomMat, 2);
+        _bloomMat.SetTexture("_BloomTex", tmp);
+        Graphics.Blit(src, dest, _bloomMat, 3);
+        
+        RenderTexture.ReleaseTemporary(tmp);
+        RenderTexture.ReleaseTemporary(tmp2);
+    }
+}
